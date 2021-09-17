e1b8c2337909a08780dca5c0bd58d5f51a7cb056
modernAlcibiades <mehul.kumar@columbia.edu>
2021-09-18 02:46:57 +0900

    Change remote before commiting anything else

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,38 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
-// The next line is part of the sample project, you don't need it in your
-// project. It imports a Hardhat task definition, that can be used for
-// testing the frontend.
-require("./tasks/faucet");
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.7.3"
+  solidity: "0.8.7",
+  networks: {
+    hardhat: {
+      forking: {
+        chainId: 250,
+        url: "https://rpc.ftm.tools/",
+        blockNumber: 17115171,
+      }
+    },
+    fantom: {
+      chainId: 250,
+      url: "https://rpc.ftm.tools/",
+      accounts: [process.env.PRIVATE_KEY],
+      maxFeePerGas: 190000000000
+    },
+  },
+  "evmVersion": "berlin"
 };
